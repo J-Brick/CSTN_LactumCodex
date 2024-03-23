dbaaf0aab83f22edd722cd9c740b0a3f1fa667d7
J-Brick <100317351+J-Brick@users.noreply.github.com>
2024-03-22 19:45:19 -0600

    Add project files.

---
+++ CSTN_LactumCodex/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CSTN_LactumCodex
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
