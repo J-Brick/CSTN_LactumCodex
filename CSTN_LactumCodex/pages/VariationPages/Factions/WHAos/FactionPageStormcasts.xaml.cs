ee485c141136ad6af0bbb8d805e607cc24991cdb
J-Brick <100317351+J-Brick@users.noreply.github.com>
2024-04-29 17:51:23 -0600

    setting up the warhammer age of sigmar faction pages

---
+++ CSTN_LactumCodex/pages/VariationPages/Factions/WHAos/FactionPageStormcasts.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSTN_LactumCodex.pages.VariationPages.Factions.WHAos
+{
+    /// <summary>
+    /// Interaction logic for FactionPageStormcasts.xaml
+    /// </summary>
+    public partial class FactionPageStormcasts : Window
+    {
+        public FactionPageStormcasts()
+        {
+            InitializeComponent();
+        }
+
+        private void BackBTN(object sender, RoutedEventArgs e)
+        {
+            Factions40kSelect F40kS = new Factions40kSelect();
+            F40kS.Show();
+            this.Close();
+        }
+    }
+}
