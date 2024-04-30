77e2ee54f227315e3198d873ae67aa0265e14797
J-Brick <100317351+J-Brick@users.noreply.github.com>
2024-04-29 18:30:53 -0600

    setting up game listing pages

---
+++ CSTN_LactumCodex/pages/GameListPage/GamePageListTwo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSTN_LactumCodex.pages.GameListPage
+{
+    /// <summary>
+    /// Interaction logic for GamePageListTwo.xaml
+    /// </summary>
+    public partial class GamePageListTwo : Window
+    {
+        public GamePageListTwo()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CSTN_LactumCodex/pages/GameListPage/PageGameList.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSTN_LactumCodex.pages.GameListPage
+{
+    /// <summary>
+    /// Interaction logic for PageGameList.xaml
+    /// </summary>
+    public partial class PageGameList : Window
+    {
+        public PageGameList()
+        {
+            InitializeComponent();
+        }
+    }
+}
