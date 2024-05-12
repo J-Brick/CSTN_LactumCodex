151616235667b9875d9526e8316cb7c776f3768a
J-Brick <100317351+J-Brick@users.noreply.github.com>
2024-05-11 18:36:23 -0600

    getting prof images working

--- CSTN_LactumCodex/pages/ProfilePage.xaml.cs
+++ CSTN_LactumCodex/pages/ProfilePage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,10 +27,35 @@ namespace CSTN_LactumCodex.pages
             InitializeComponent();
         }
 
-        
+        DataTable DataTab = new DataTable();
+        Hashtable HB = new Hashtable();
+        string query;
+        DataRow rows;
+        string sourceURI;
+
 
         private void ChangeImage_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog Openfile = new OpenFileDialog();
+            Openfile.Filter = "Image Files|*.bmp;*.jpg*.png";
+            Openfile.FilterIndex = 1;
+            if (Openfile.ShowDialog() == true)
+            {
+                ProfPicture.Source = new BitmapImage(new Uri(Openfile.FileName));
+                sourceURI = Openfile.FileName;
+            }
+            HB.Add("@profSource",ProfPicture.Source);
+            HB.Add("@userName",UsernameBX.Text);
+            query = "SELECT * from  where UserName = @userName";
+            DataTab = ExDB.GetDataTable("CapstoneDBs", HB, query);
+
+            if (DataTab != null) { }
+
+            rows = DataTab.Rows[0];
+
+/*            string userNM = (string)rows["UserName"];
+            string userPI = (string)rows["UserProfImg"];*/
+            
 
         }
     }
